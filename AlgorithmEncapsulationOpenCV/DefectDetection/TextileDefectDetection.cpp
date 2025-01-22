93d9c9189d9dd071ca0683904e146ea226dbdfe4
Wei Li <weili_yzzcq@163.com>
2025-01-22 22:19:39 +0800

    feat(AlgotithmEncapsulaation): Cpp findContours

--- AlgorithmEncapsulationOpenCV/DefectDetection/TextileDefectDetection.cpp
+++ AlgorithmEncapsulationOpenCV/DefectDetection/TextileDefectDetection.cpp
@@ -12,6 +12,8 @@
  * 
  */
 
+#include "opencv2/opencv.hpp"
+
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -33,7 +35,33 @@ int main(int /* argc */, const char ** /* argv */)
     SetConsoleOutputCP(CP_UTF8);
 #endif // _WIN32
 
-    
+    // # -------------------------------------------------------
+    // 脏污缺陷图片, 肉眼可见明显的几处脏污
+    cv::Mat img = cv::imread("./images/smudge.png", cv::IMREAD_UNCHANGED);
+    // 【1】使用高斯滤波消除背景纹理的干扰, 将原图放大后会发现纺织物自带的纹理比较明显,
+    // 这会影响后续处理结果, 所以先做滤波平滑
+    cv::Mat gray;
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    cv::Mat blur;
+    cv::GaussianBlur(gray, blur, cv::Size(7, 7), 0);
+    cv::imwrite("./images/smudge_blur.png", blur);
+    // 【2】Canny边缘检测凸显缺陷,
+    // Canny边缘检测对低对比度缺陷检测有很好的效果, 这里注意高低阈值的设置
+    cv::Mat edged;
+    cv::Canny(blur, edged, 10, 30);
+    cv::imwrite("./images/smudge_edged.png", edged);
+    // 【3】轮廓查找、筛选与结果标记,
+    // 轮廓筛选可以根据面积、长度过滤掉部分干扰轮廓, 找到真正的缺陷
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i>              hierarchy;
+    cv::findContours(edged, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    for (const auto &cnt : contours)
+    {
+        auto length = cv::arcLength(cnt, true);
+        if (length > 1.0)
+            cv::drawContours(img, contours, -1, (255, 0, 0), 2, 8, hierarchy);
+    }
+    cv::imwrite("./images/smudge_defect.png", img);
 
     return 0;
 }
