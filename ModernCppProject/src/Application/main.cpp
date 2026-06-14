2fbcb00f6900a7156d0c8db1efd3b83ed44a1ffe
Wei Li <weili_yzzcq@163.com>
2026-06-14 20:04:41 +0800

    feat(ModernCppProject): update Project info

--- ModernCppProject/src/Application/main.cpp
+++ ModernCppProject/src/Application/main.cpp
@@ -1,8 +1,16 @@
 #include "Application/Application.hpp"
+#include "Version/version.h"
+
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     Application::getInstance().Run();
 
+    std::cout << "Version NO: " << Ithaca::version::VERSION_MAJOR << "." << Ithaca::version::VERSION_MINOR << "."
+              << Ithaca::version::VERSION_PATCH << std::endl;
+    std::cout << "Version: " << Ithaca::version::VERSION_STRING << std::endl;
+    std::cout << "Build Time: " << Ithaca::version::BUILD_TIMESTAMP << std::endl;
+    std::cout << "Version-Build Time: " << Ithaca::version::VERSION_BUILD_STRING << std::endl;
     return 0;
 }
